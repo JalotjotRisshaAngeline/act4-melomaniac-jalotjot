d7bf39f1f5c2620fa0dcb2be4c43a17c39ed7e00
JalotjotRisshaAngeline <jalotjotrisshaangeline@gmail.com>
2022-05-17 20:02:03 +0800

    song2

---
+++ song2.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>BTS TRACK</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Welcome to BTS Track </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to BTS Track</h1>
+                <p class="lead">BTS Track is here to give comfort and relax your mind. A peaceful music that can make you feel you're in paradise.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+<?php
+                        echo "<h2 class='text-primary'>My Universe</h2>";
+                        echo "<p class='lead'>BTS</p>";
+                        
+
+                        echo "<p>You (you), you are (you are) my universe</p>";
+                        echo "<p>And I (and I) just want (just want) to put you first</p>";
+                        echo "<p>And you (you), you are (you are) my universe, and I</p>";
+
+                        echo "<p>In the night, I lie and look up at you</p>";
+                        echo "<p>When the morning comes, I watch you rise</p>";
+                        echo "<p>There's a paradise they couldn't capture</p>";
+                        echo "<p>That bright infinity inside your eyes</p>";
+
+                        echo "<p>Maeil bam nege naraga (ga)</p>";
+                        echo "<p>Kkumiran geotdo ijeun chae</p>";
+                        echo "<p>Na useumyeo neoreul manna (na)</p>";
+                        echo "<p>Never ending forever, baby</p>";
+
+                        echo "<p>You (you), you are (you are) my universe</p>";
+                        echo "<p>And I (I), just want (just want) to put you first (to put you first)</p>";
+                        echo "<p>And you (you), you are (you are) my universe</p>";
+                        echo "<p>And you make my world light up inside</p>";
+
+                        echo "<p>Eodumi naegen deo pyeonhaesseotji</p>";
+                        echo "<p>Gireojin geurimja sogeseo (eyes)</p>";
+                        echo "<p>And they said that we can't be together</p>";
+                        echo "<p>Because, because we come from different sides</p>";
+
+                        echo "<p>You (you), you are (you are) my universe</p>";
+                        echo "<p>And I (I), just want (just want) to put you first (to put you first)</p>";
+                        echo "<p>And you (you), you are (you are) my universe</p>";
+                        echo "<p>And you make my world light up inside</p>";
+
+                        echo "<p>My universe (do-do, do-do)</p>";
+                        echo "<p>My universe (do-do, do-do)</p>";
+                        echo "<p>My universe (do-do, do-do)</p>";
+                        echo "<p>(You make my world)</p>";
+                        echo "<p>You make my world light up inside</p>";
+                        echo "<p>Make my world light up inside</p>";
+
+                        echo "<p>Nareul balkyeojuneun geon</p>";
+                        echo "<p>Neoran sarangeuro su noajin byeol</p>";
+                        echo "<p>Nae ujuui neon</p>";
+                        echo "<p>Tto dareun sesangeul mandeureo juneun geol</p>";
+                        echo "<p>Neoneun nae byeorija naui ujunikka</p>";
+                        echo "<p>Jigeum i siryeondo gyeolgugen jamsinikka</p>";
+                        echo "<p>Neoneun eonjekkajina jigeumcheoreom balgeman binnajwo</p>";
+                        echo "<p>Urineun neoreul ttara i gin bameul sunoeul geoya</p>";
+
+                        echo "<p>Neowa hamkke naraga</p>";
+                        echo "<p>When I'm without you I'm crazy</p>";
+                        echo "<p>Ja eoseo nae soneul jaba</p>";
+                        echo "<p>We are made of each other, baby</p>";
+
+                        echo "<p>You (you), you are (you are) my universe</p>";
+                        echo "<p>And I (I), just want (just want) to put you first (to put you first)</p>";
+                        echo "<p>And you (you), you are (you are) my universe</p>";
+                        echo "<p>And you make my world light up inside</p>";
+
+                        echo "<p>My universe (do-do, do-do)</p>";
+                        echo "<p>You, you are</p>";
+                        echo "<p>My universe (do-do, do-do)</p>";
+                        echo "<p>I just want</p>";
+                        echo "<p>My universe</p>";
+                        echo "<p>You, you are my universe, and I (My universe)</p>";
+                        ?>
+                    
+</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rissha Angeline Jalotjot</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
