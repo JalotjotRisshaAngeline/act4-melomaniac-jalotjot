80459fcf7d801878ec6e8b8517feb018e40036b0
JalotjotRisshaAngeline <jalotjotrisshaangeline@gmail.com>
2022-05-17 20:00:20 +0800

    song1

---
+++ song1.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>BTS TRACK</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Welcome to BTS Track </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to BTS Track</h1>
+                <p class="lead">BTS Track is here to give comfort and relax your mind. A peaceful music that can make you feel you're in paradise.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        
+<?php
+                        echo "<h2 class='text-primary'>Dynamite</h2>";
+                        echo "<p class='lead'>BTS</p>";
+
+                        echo "<p>Cause, ah-ah, I'm in the stars tonight</p>";
+                        echo "<p>So watch me bring the fire and set the night alight</p>";
+                        echo "<p>Shoes on, get up in the morning, Cup of milk, let's rock and roll</p>";
+                        echo "<p>King Kong, kick the drum, rolling on like a rolling stone</p>";
+                        echo "<p>Sing song when I'm walking home</p>";
+                        echo "<p>Jump up to the top, LeBron</p>";
+                        echo "<p>Ding dong, call me on my phone</p>";
+                        echo "<p>Ice tea and a game of ping pong</p>";
+                        echo "<br/>";
+
+                        echo "<p>This is getting heavy</p>";
+                        echo "<p>Can you hear the bass boom? I'm ready</p>";
+                        echo "<p>Life is sweet as honey</p>";
+                        echo "<p>Yeah, this beat cha ching like money</p>";
+                        echo "<p>Disco overload, I'm into that, I'm good to go</p>";
+                        echo "<p>I'm diamond, you know I glow up</p>";
+                        echo "<p>Hey, so let's go</p>";
+                        echo "<br/>";
+
+                        echo "<p>'Cause, ah-ah, I'm in the stars tonight</p>";
+                        echo "<p>So watch me bring the fire and set the night alight</p>";
+                        echo "<p>Shining through the city with a little funk and soul</p>";
+                        echo "<p>So I'ma light it up like dynamite, whoa</p>";
+                        echo "<br/>";
+
+                        echo "<p>Bring a friend, join the crowd</p>";
+                        echo "<p>Whoever wanna come along</p>";
+                        echo "<p>Word up, talk the talk, just move like we off the wall</p>";
+                        echo "<p>Day or night the sky's alight</p>";
+                        echo "<p>So we dance to the break of dawn</p>";
+                        echo "<p>Ladies and gentlemen, I got the medicine, so you should keep ya eyes on the ball, huh</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>This is getting heavy</p>";
+                        echo "<p>Can you hear the bass boom? I'm ready</p>";
+                        echo "<p>Life is sweet as honey</p>";
+                        echo "<p>Yeah, this beat cha ching like money</p>";
+                        echo "<p>Disco overload, I'm into that, I'm good to go</p>";
+                        echo "<p>I'm diamond, you know I glow up</p>";
+                        echo "<p>Let's go</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>'Cause, ah-ah, I'm in the stars tonight</p>";
+                        echo "<p>So watch me bring the fire and set the night alight</p>";
+                        echo "<p>Shining through the city with a little funk and soul</p>";
+                        echo "<p>So I'ma light it up like dynamite, whoa</p>";
+
+                        echo "<p>Dy-na-na-na, na-na, na-na-na, na-na, life is dynamite</p>";
+                        echo "<p>Dy-na-na-na, na-na, na-na-na, na-na, life is dynamite</p>";
+                        echo "<p>Shining through the city with a little funk and soul</p>";
+                        echo "<p>So I'ma light it up like dynamite, whoa</p>";
+                        echo "<br/>";
+
+                        echo "<p>Dy-na-na-na, na-na, na-na, eh</p>";
+                        echo "<p>Dy-na-na-na, na-na, na-na, eh</p>";
+                        echo "<p>Dy-na-na-na, na-na, na-na, eh</p>";
+                        echo "<p>Light it up, dynamite</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>Dy-na-na-na, na-na, na-na, eh</p>";
+                        echo "<p>Dy-na-na-na, na-na, na-na, eh</p>";
+                        echo "<p>Dy-na-na-na, na-na, na-na, eh</p>";
+                        echo "<p>Light it up, dynamite</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>'Cause, ah-ah, I'm in the stars tonight</p>";
+                        echo "<p>So watch me bring the fire and set the night alight</p>";
+                        echo "<p>Shining through the city with a little funk and soul</p>";
+                        echo "<p>So I'ma light it up like dynamite</p>";
+                        echo "<br/>";
+                        ?>
+
+
+</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rissha Angeline Jalotjot</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
