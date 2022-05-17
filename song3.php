9ff8d903736531bb6b0ec79a5f5478de3075dfc3
JalotjotRisshaAngeline <jalotjotrisshaangeline@gmail.com>
2022-05-17 20:03:53 +0800

    song3

---
+++ song3.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>BTS TRACK</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Welcome to BTS Track </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to BTS Track</h1>
+                <p class="lead">BTS Track is here to give comfort and relax your mind. A peaceful music that can make you feel you're in paradise.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+<?php
+                        echo "<h2 class='text-primary'>Butter</h2>";
+                        echo "<p class='lead'>BTS</p>";
+
+                        echo "<p>Smooth like butter</p>";
+                        echo "<p>Like a criminal undercover</p>";
+                        echo "<p>Gon' pop like trouble</p>";
+                        echo "<p>Breakin' into your heart like that (Ooh)</p>";
+                        echo "<p>Cool shade stunner</p>";
+                        echo "<p>Yeah, I owe it all to my mother</p>";
+                        echo "<p>Hot like summer</p>";
+                        echo "<p>Yeah, I'm makin' you sweat like that</p>";
+                        echo "<p>Break it down</p>";
+
+                        echo "<p>Oh, when I look in the mirror</p>";
+                        echo "<p>I'll melt your heart into two</p>";
+                        echo "<p>I got that superstar glow so (Ooh)</p>";
+                        echo "<p>Do the boogie like</p>";
+
+                        echo "<p>Side step, right, left to my beat</p>";
+                        echo "<p>High like the moon, rock with me, baby</p>";
+                        echo "<p>Know that I got that heat</p>";
+                        echo "<p>Let me show you 'cause talk is cheap</p>";
+                        echo "<p>Side step, right, left to my beat</p>";
+                        echo "<p>Get it, let it roll</p>";
+
+                        echo "<p>Pull you in like no other</p>";
+                        echo "<p>Don't need no Usher</p>";
+                        echo "<p>To remind me you got it bad</p>";
+                        echo "<p>Ain't no other</p>";
+                        echo "<p>That can sweep you up like a robber</p>";
+                        echo "<p>Straight up, I got ya</p>";
+                        echo "<p>Makin' you fall like that</p>";
+                        echo "<p>Break it down</p>";
+
+                        echo "<p>Oh, when I look in the mirror</p>";
+                        echo "<p>I'll melt your heart into two</p>";
+                        echo "<p>I got that superstar glow so (Ooh)</p>";
+                        echo "<p>Do the boogie like</p>";
+
+                        echo "<p>Side step, right, left to my beat</p>";
+                        echo "<p>High like the moon, rock with me, baby</p>";
+                        echo "<p>Know that I got that heat</p>";
+                        echo "<p>Let me show you 'cause talk is cheap</p>";
+                        echo "<p>Side step, right, left to my beat</p>";
+                        echo "<p>Get it, let it roll</p>";
+
+                        echo "<p>Get it, let it roll</p>";
+                        echo "<p>Get it, let it roll</p>";
+
+                        echo "<p>Ice on my wrist, I'm the nice guy</p>";
+                        echo "<p>Got the right body and the right mind</p>";
+                        echo "<p>Rollin' up to party, got the right vibe</p>";
+                        echo "<p>Smooth like (Butter), hate us (Love us)</p>";
+                        echo "<p>Fresh boy pull up and we lay low</p>";
+                        echo "<p>All the playas get movin' when the bass low</p>";
+                        echo "<p>Got ARMY right behind us when we say so</p>";
+                        echo "<p>Let's go</p>";
+
+                        echo "<p>Side step, right, left to my beat (Right, left to my beat)</p>";
+                        echo "<p>High like the moon, rock with me, baby</p>";
+                        echo "<p>Know that I got that heat</p>";
+                        echo "<p>Let me show you 'cause talk is cheap (You know that talk is cheap)</p>";
+                        echo "<p>Side step, right, left to my beat</p>";
+                        echo "<p>Get it, let it roll</p>";
+
+                        echo "<p>Smooth like (Butter), cool shade (Stunner)</p>";
+                        echo "<p>And you know we don't stop</p>";
+                        echo "<p>Hot like (Summer), ain't no (Bummer)</p>";
+                        echo "<p>You be like, oh my God</p>";
+                        echo "<p>We gon' make you rock and you say (Yeah)</p>";
+                        echo "<p>We gon' make you bounce and you say (Yeah)</p>";
+                        echo "<p>Hotter? Sweeter! Cooler? Butter!</p>";
+
+                        echo "<p>Get it, let it roll</p>";                        
+                        ?>
+
+</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rissha Angeline Jalotjot</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
