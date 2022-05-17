fa40f685974d2e093a2f81510522d8310ff780be
JalotjotRisshaAngeline <jalotjotrisshaangeline@gmail.com>
2022-05-17 20:05:51 +0800

    song4

---
+++ song4.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>BTS TRACK</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Welcome to BTS Track </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to BTS Track</h1>
+                <p class="lead">BTS Track is here to give comfort and relax your mind. A peaceful music that can make you feel you're in paradise.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+<?php
+                        echo "<h2 class='text-primary'>Permission To Dance</h2>";
+                        echo "<p class='lead'>BTS</p>";                        
+
+                        echo "<p>It's the thought of being young</p>";
+                        echo "<p>When your heart's just like a drum</p>";
+                        echo "<p>Beating louder with no way to guard it</p>";
+                        echo "<p>When it all seems like it's wrong</p>";
+                        echo "<p>Just sing along to Elton John</p>";
+                        echo "<p>And to that feeling, we're just getting started</p>";
+
+                        echo "<p>When the nights get colder</p>";
+                        echo "<p>And the rhythms got you falling behind</p>";
+                        echo "<p>Just dream about that moment</p>";
+                        echo "<p>When you look yourself right in the eye, eye, eye</p>";
+                        echo "<p>Then you say</p>";
+
+                        echo "<p>I wanna dance</p>";
+                        echo "<p>The music's got me going</p>";
+                        echo "<p>Ain't nothing that can stop how we move, yeah</p>";
+                        echo "<p>Let's break our plans</p>";
+                        echo "<p>And live just like we're golden</p>";
+                        echo "<p>And roll in like we're dancing fools</p>";
+
+                        echo "<p>We don't need to worry</p>";
+                        echo "<p>'Cause when we fall, we know how to land</p>";
+                        echo "<p>Don't need to talk the talk</p>";
+                        echo "<p>Just walk the walk tonight</p>";
+                        echo "<p>'Cause we don't need Permission to Dance</p>";
+
+                        echo "<p>There's always something that's standing in the way</p>";
+                        echo "<p>But if you don't let it faze ya</p>";
+                        echo "<p>You'll know just how to break</p>";
+                        echo "<p>Just keep the right vibe, yeah</p>";
+                        echo "<p>'Cause there's no looking back</p>";
+                        echo "<p>There ain't no one to prove</p>";
+                        echo "<p>We don't got this on lock, yeah</p>";
+
+                        echo "<p>The wait is over</p>";
+                        echo "<p>The time is now</p>";
+                        echo "<p>So let's do it right</p>";
+                        echo "<p>Yeah, we'll keep going</p>";
+                        echo "<p>And stay up until we see the sunrise</p>";
+                        echo "<p>And we'll say</p>";
+
+                        echo "<p>I wanna dance</p>";
+                        echo "<p>The music's got me going</p>";
+                        echo "<p>Ain't nothing that can stop how we move, yeah</p>";
+                        echo "<p>Let's break our plans</p>";
+                        echo "<p>And live just like we're golden</p>";
+                        echo "<p>And roll in like we're dancing fools</p>";
+
+                        echo "<p>We don't need to worry</p>";
+                        echo "<p>'Cause when we fall, we know how to land</p>";
+                        echo "<p>Don't need to talk the talk</p>";
+                        echo "<p>Just walk the walk tonight</p>";
+                        echo "<p>'Cause we don't need Permission to Dance</p>";
+
+                        echo "<p>Da-na-na-na-na-na-na</p>";
+                        echo "<p>Da-na-na-na-na-na-na</p>";
+                        echo "<p>Da-na-na-na-na-na-na</p>";
+                        echo "<p>No, we don't need Permission to Dance</p>";
+
+                        echo "<p>Da-na-na-na-na-na-na</p>";
+                        echo "<p>Da-na-na-na-na-na-na</p>";
+                        echo "<p>Da-na-na-na-na-na-na</p>";
+
+                        echo "<p>Well, let me show ya</p>";
+                        echo "<p>That we can keep the fire alive</p>";
+                        echo "<p>'Cause it's not over</p>";
+                        echo "<p>'Till it's over, say it one more time</p>";
+                        echo "<p>Say</p>";
+
+                        echo "<p>I wanna dance</p>";
+                        echo "<p>The music's got me going</p>";
+                        echo "<p>Ain't nothing that can stop how we move, yeah</p>";
+                        echo "<p>Let's break our plans</p>";
+                        echo "<p>And live just like we're golden</p>";
+                        echo "<p>And roll in like we're dancing fools</p>";
+
+                        echo "<p>We don't need to worry</p>";
+                        echo "<p>'Cause when we fall, we know how to land</p>";
+                        echo "<p>Don't need to talk the talk</p>";
+                        echo "<p>Just walk the walk tonight</p>";
+                        echo "<p>'Cause we don't need Permission to Dance</p>";
+                        ?>
+</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rissha Angeline Jalotjot</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
