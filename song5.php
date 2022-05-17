b823f72578c9ef43ab9c7db3b225b6f765cb646c
JalotjotRisshaAngeline <jalotjotrisshaangeline@gmail.com>
2022-05-17 20:07:39 +0800

    song5

---
+++ song5.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>BTS TRACK</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Welcome to BTS Track </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to BTS Track</h1>
+                <p class="lead">BTS Track is here to give comfort and relax your mind. A peaceful music that can make you feel you're in paradise.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        
+<?php
+                        echo "<h2 class='text-primary'>Life Goes On</h2>";
+                        echo "<p class='lead'>BTS</p>"; 
+
+                        echo "<p>(I remember)</p>";
+echo "<p>(Ah, yeah-yeah-yeah-yeah)</p>";
+echo "<p>(I remember)</p>";
+echo "<p>(Ah, ah)</p>";
+
+echo "<p>Eoneu nal sesangi meomchwosseo</p>";
+echo "<p>Amureon yegodo hana eopsi</p>";
+echo "<p>Bomeun gidarimeul mollaseo</p>";
+echo "<p>Nunchi eopsi wabeoryeosseo</p>";
+echo "<p>Baljagugi jiwojin geori</p>";
+echo "<p>Yeogi neomeojyeoinneun na</p>";
+echo "<p>Honja gane sigani</p>";
+echo "<p>Mianhae maldo eopsi (Yeah)</p>";
+
+echo "<p>Oneuldo biga naeril geot gata</p>";
+echo "<p>Heumppeok jeojeobeoryeonne</p>";
+echo "<p>Ajikdo meomchujil ana</p>";
+echo "<p>Jeo meokgureumboda ppalli dallyeoga</p>";
+echo "<p>Geureom doel jul aranneunde</p>";
+echo "<p>Na gyeou saraminga bwa</p>";
+echo "<p>Mopsi apeune</p>";
+echo "<p>Sesangiran nomi jun gamgi</p>";
+echo "<p>Deokbune nulleoboneun meonji ssain doegamgi</p>";
+echo "<p>Neomeojin chae cheonghaneun eotbakjaui chum</p>";
+echo "<p>Gyeouri omyeon naeswija</p>";
+echo "<p>Deo tteugeoun sum</p>";
+
+echo "<p>Kkeuchi boiji ana</p>";
+echo "<p>Chulguga itgin halkka</p>";
+echo "<p>Bari ttejijil ana ana oh</p>";
+echo "<p>Jamsi du nuneul gama</p>";
+echo "<p>Yeogi nae soneul jaba</p>";
+echo "<p>Jeo miraero daranaja</p>";
+
+echo "<p>Like an echo in the forest</p>";
+echo "<p>Haruga doraogetji</p>";
+echo "<p>Amu ildo eopdan deusi</p>";
+echo "<p>Yeah, life goes on</p>";
+echo "<p>Like an arrow in the blue sky</p>";
+echo "<p>Tto haru deo naragaji</p>";
+echo "<p>On my pillow, on my table</p>";
+echo "<p>Yeah, life goes on</p>";
+echo "<p>Like this again</p>";
+
+echo "<p>I eumageul billyeo neoege na jeonhalge</p>";
+echo "<p>Saramdeureun malhae sesangi da byeonhaetdae (hm, hm, hm, hmm)</p>";
+echo "<p>Dahaenghido uri saineun</p>";
+echo "<p>Ajik yeotae an byeonhaenne</p>";
+
+echo "<p>Neul hadeon sijakgwa kkeut 'annyeong'iran mallo</p>";
+echo "<p>Oneulgwa naeireul tto hamkke ieobojago</p>";
+echo "<p>Meomchwoitjiman eodume sumji ma</p>";
+echo "<p>Bicheun tto tteooreunikkan</p>";
+
+echo "<p>Kkeuchi boiji ana</p>";
+echo "<p>Chulguga itgin halkka</p>";
+echo "<p>Bari ttejijil ana ana oh</p>";
+echo "<p>Jamsi du nuneul gama</p>";
+echo "<p>Yeogi nae soneul jaba (jaba)</p>";
+echo "<p>Jeo miraero daranaja (Oh, oh, oh)</p>";
+
+echo "<p>Like an echo in the forest</p>";
+echo "<p>Haruga doraogetji</p>";
+echo "<p>Amu ildo eopdan deusi</p>";
+echo "<p>Yeah, life goes on</p>";
+echo "<p>Like an arrow in the blue sky</p>";
+echo "<p>Tto haru deo naragaji</p>";
+echo "<p>On my pillow, on my table</p>";
+echo "<p>Yeah, life goes on</p>";
+echo "<p>Like this again</p>";
+
+echo "<p>I remember, ah-yeah, yeah, yeah, yeah</p>";
+echo "<p>I remember, ah-ah-ah, ah-ah-ah</p>";
+echo "<p>I remember, ah-yeah, yeah, yeah, yeah</p>";
+echo "<p>I remember, ah-ah-ah, ah-ah-ah</p>";
+?>
+
+</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Rissha Angeline Jalotjot</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
